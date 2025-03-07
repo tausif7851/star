1f8314ed4218a616b5d745b5667fdfc3caab91f6
Mohamed Tousif <mohamedtousif141@gmail.com>
2025-03-07 11:26:52 +0530

    Update SideBar.jsx

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -14,33 +14,26 @@ const Sidebar = () => {
         setIsSidebarClosed(!isSidebarClosed);
     };
 
-    // Function to toggle dark mode
-    // const toggleDarkMode = () => {
-    //     setIsDarkMode(!isDarkMode);
-    //     localStorage.setItem("theme", "light");
-    // };
+    const buttonStyle = {
+        background: !isDarkMode ? `url('https://studioai.lenovo.com/asset/home/screen-banner.webp')` : '',
+        backgroundSize: !isDarkMode ? "cover" : '',
+        backgroundPosition: !isDarkMode ? "center" : '',
+        backgroundRepeat: !isDarkMode ? "no-repeat" : '',
 
-    // Effect to change body class based on dark mode state
-    // React.useEffect(() => {
-    //     if (isDarkMode) {
-    //         document.body.classList.add('dark');
-    //     } else {
-    //         document.body.classList.remove('dark');
-    //     }
-    // }, [isDarkMode]);
+    };
 
     return (
-        <div>
-            <nav className={`sidebar ${isSidebarClosed ? 'close' : ''}`}>
+        <div >
+            <nav className={`sidebar ${isSidebarClosed ? 'close' : ''}`} style={buttonStyle}>
                 <header>
                     <div className="image-text">
                         <span className="image">
-                            <img src="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBwkHBgkNCgoIBwkLDRYPDQwHDQ8UFRAWIB0cIiAdHx8kKDQsJCExJx8TLUktMTU3Ojo6Iys/RD86QzQ5PjcBCgoKDQwNGg8PGjclHyU3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3NTc3Nzc4Kzc3Nzc4MissKy83LTc3ODErNSs4Nzc4Lf/AABEIABwAHAMBEQACEQEDEQH/xAAXAAADAQAAAAAAAAAAAAAAAAABBgcA/8QAKxAAAQMDAQMNAAAAAAAAAAAAAQACAwQFESEGEnEHExUWIjEyNlFhc4HC/8QAGwEAAQUBAQAAAAAAAAAAAAAAAAEDBAUGAgf/xAAmEQACAgAEBAcAAAAAAAAAAAAAAQIDBBESMQUhQVETFDI0YdHw/9oADAMBAAIRAxEAPwBeOgKoj05DNtTszBZLXbJ45553VjcubMGYb2QdMD3T9tKhFNPcrMDxGeKusrlFLT9iymCyZkomQH+E8EHUdyh8o/lvZ34/w1TMV6Imc4L7q/8AdWT1QzRMyQVAdqDwQItyrXLq5f7Na4qu80tI6mibpT1MAOd0Ag5z6Kxn4VkUnLYydHn8JfZOulvU+qff4FHaezWK3UEcltuvSlQ6YMdHz0D8M3XHOGgHvDR9qNbXXFZwlmXOBxeMuscb6tKy3ya58u4sKOWYSMIACBQ4QDMu0htvI//Z" alt="" style={{ cursor: "pointer" }} onClick={toggleSidebar} />
+                            {!isSidebarClosed ? <img src="https://p4-ofp.static.pub/fes/cms/2022/11/14/h82es5y402b4rh1089sf86ay7n9sdl721044.png" alt="" onClick={toggleSidebar} /> : <img src="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBwkHBgkNCgoIBwkLDRYPDQwHDQ8UFRAWIB0cIiAdHx8kKDQsJCExJx8TLUktMTU3Ojo6Iys/RD86QzQ5PjcBCgoKDQwNGg8PGjclHyU3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3NTc3Nzc4Kzc3Nzc4MissKy83LTc3ODErNSs4Nzc4Lf/AABEIABwAHAMBEQACEQEDEQH/xAAXAAADAQAAAAAAAAAAAAAAAAABBgcA/8QAKxAAAQMDAQMNAAAAAAAAAAAAAQACAwQFESEGEnEHExUWIjEyNlFhc4HC/8QAGwEAAQUBAQAAAAAAAAAAAAAAAAEDBAUGAgf/xAAmEQACAgAEBAcAAAAAAAAAAAAAAQIDBBESMQUhQVETFDI0YdHw/9oADAMBAAIRAxEAPwBeOgKoj05DNtTszBZLXbJ45553VjcubMGYb2QdMD3T9tKhFNPcrMDxGeKusrlFLT9iymCyZkomQH+E8EHUdyh8o/lvZ34/w1TMV6Imc4L7q/8AdWT1QzRMyQVAdqDwQItyrXLq5f7Na4qu80tI6mibpT1MAOd0Ag5z6Kxn4VkUnLYydHn8JfZOulvU+qff4FHaezWK3UEcltuvSlQ6YMdHz0D8M3XHOGgHvDR9qNbXXFZwlmXOBxeMuscb6tKy3ya58u4sKOWYSMIACBQ4QDMu0htvI//Z" alt="" style={{ cursor: "pointer" }} onClick={toggleSidebar} />}
                         </span>
-                        <div className="text logo-text-1">
+                        {/* <div className="text logo-text-1">
                             <span className="name">Studio AI</span>
                             <span className="profession">AI Smart Engine</span>
-                        </div>
+                        </div> */}
                     </div>
                     <i
                         className="bx bx-chevron-right toggle"
@@ -79,10 +72,10 @@ const Sidebar = () => {
                         </li>
                         <li className="mode">
                             <div className="sun-moon">
-                                <i className="bx bx-moon icon moon"></i>
+                                <i className={`bx bx-moon icon moon ${!isDarkMode ? 'sidebar-last-text' : ''}`}></i>
                                 <i className="bx bx-sun icon sun"></i>
                             </div>
-                            <span className="mode-text text">
+                            <span className={`mode-text text ${!isDarkMode ? 'sidebar-last-child' : ''}`}>
                                 {isDarkMode ? 'Light mode' : 'Dark mode'}
                             </span>
                             <div className="toggle-switch" onClick={toggleDarkMode}>
